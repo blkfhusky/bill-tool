04a65dfdbcff5260420c59ac5303ff9cf440fd32
zazuken <zxc819578393@foxmail.com>
2018-12-02 21:00:45 +0800

    eureka server

---
+++ bill-tool-eureka-server/src/main/java/com/blkfhusky/eureka/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.blkfhusky.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author blkfhusky
+ *
+ * eureka server
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
